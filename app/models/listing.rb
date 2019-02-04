c7b8b4dfa8aa6d5d2e33e5557f06c331a4ddf347
github username <github email address>
2019-02-04 18:12:23 -0500

    it works

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,1 +1,52 @@
+require 'pry'
 
+class Listing
+
+  attr_reader :city
+
+  @@all = []
+
+  def initialize(city)
+    @city = city
+
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def trips
+    Trip.all.select do |trip|
+      trip.listing = self
+    end
+  end
+
+  def guests
+    trips.map do |trip|
+      trip.guest
+    end
+  end
+
+  def trip_count
+    trips.length
+  end
+
+  def self.find_all_by_city(city_name)
+    self.all.select do |list|
+      list.city == city_name
+    end
+  end
+
+  def self.most_popular
+    count = Hash.new(0)
+    Trip.all.select do |trip|
+      count[trip.listing] += 1
+    end
+    sorted = count.sort_by do |city, cnt|
+      cnt
+    end
+    sorted[-1][0]
+  end
+
+end
--- tools/console.rb
+++ tools/console.rb
@@ -4,9 +4,27 @@ def reload
   load 'config/environment.rb'
 end
 
+listing0 = Listing.new("New York")
+listing1 = Listing.new("Portland")
+listing2 = Listing.new("Seattle")
+listing3 = Listing.new("Boston")
 
+guest1 = Guest.new("Seann")
+guest2 = Guest.new("Doc")
+guest3 = Guest.new("Joe")
+guest4 = Guest.new("Jane")
+
+trip0 = Trip.new(guest1, listing3, 8)
+trip1 = Trip.new(guest1, listing1, 7)
+trip2 = Trip.new(guest2, listing1, 5)
+trip3 = Trip.new(guest3, listing2, 3)
+trip4 = Trip.new(guest4, listing1, 3)
+trip5 = Trip.new(guest4, listing0, 3)
+trip6 = Trip.new(guest4, listing1, 3)
+trip7 = Trip.new(guest2, listing0, 3)
 
 
 
 # Put your variables here~!
 binding.pry
+0
